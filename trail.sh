5afa996cf8c90cd7160589db47d88da21c38ff69
aqsa286 <aqsafarooq402@gmail.com>
2023-06-06 16:46:09 +0500

    My trail script

---
+++ trail.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+# generate URL.sh script
+
+script=/home/aqsa286/Desktop/URL.sh         #name of the script
+
+(
+cat << 'EOF'
+#! /bin/bash
+
+URL=https://www.google.com.pk
+echo "URL=" $URL
+
+Status_Code=$(curl --write-out "%{http_code}" $URL --output output.txt --silent )
+
+if [ $Status_Code -eq 200 ]
+then
+  echo " URL is working.Status Code=$Status_Code "
+else
+  echo " URL is not working.Status Code=$Status_Code"
+fi     
+
+EOF
+) >  $script  
+
+chmod +x $script
+sh $script 
+
+crontab <<EOF
+*/5 * * * * $script
+EOF
